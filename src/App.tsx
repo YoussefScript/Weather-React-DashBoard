a34e27df0f40ffc212c1eb9abd41ef020b933235
youssef <youssef1362009kamel@gmail.com>
2026-02-06 23:06:20 +0200

    setupapi

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import './index.css';
+import { useQuery } from '@tanstack/react-query';
+import { getWeather } from './api';
 
 function App() {
 
+  const {data} = useQuery({
+    queryKey: ['weather'],
+    queryFn: () => getWeather({lat: '50', lon: '50'}),
+  })
+
   return (
     <>
-      
+      <div>{JSON.stringify(data)}</div>
     </>
   );
 };
---
+++ src/api.ts
@@ -0,0 +1,7 @@
+const API_KEY = import.meta.env.VITE_API_KEY;
+
+export async function getWeather({lat, lon}: {lat: string, lon: string}) {
+    const res = await fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude={minutely,alerts}&appid=${API_KEY}`);
+    const data = await res.json();
+    return data;
+}
